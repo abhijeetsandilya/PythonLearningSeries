3614d498834394e9b49ab90509d1df0dd9f2d5bb
Abhijeet Sandilya <abhijeetsandilya8@gmail.com>
2025-11-06 19:52:12 +0530

    Update movie_guesser.py

--- movie_guesser.py
+++ movie_guesser.py
@@ -24,37 +24,3 @@ if user_guess.lower() == tit.lower():
     print("correct")
 else:
     print("You're Wrong!")
-
-# import random
-#
-# movies = []
-#
-# for i in range(3):
-#     movies.append(input("Enter movie name:\n"))
-#
-# pholder = input("Enter your placeholder:\n")
-#
-# tit = movies[random.randint(0, 2)]
-#
-#
-# def obfuscate_title(title, pholder):
-#     title_list = list(title)
-#     n = int(0.7 * len(title))
-#
-#     positions = random.sample(range(len(title)), n)
-#
-#     for pos in positions:
-#         title_list[pos] = pholder
-#
-#     return "".join(title_list)
-#
-#
-# new_tit = obfuscate_title(tit, pholder)
-#
-# print("Mystery word is:", new_tit)
-#
-# user_guess = input("Guess The Name!\n")
-# if user_guess == tit:
-#     print("Correct!")
-# else:
-#     print("You're Wrong! The correct movie was:", tit)
